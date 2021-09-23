7f7690384e7f18416013cc41a90fddfbe9251fa8
cymande0 <konradpietapl@gmail.com>
2021-09-23 21:25:42 +0200

    Intro to JS

---
+++ 10d_js_intro/js/events_actions.js
@@ -0,0 +1,58 @@
+const btn = document.querySelector('button');
+const paragraph = document.querySelector('p');
+
+// Element.addEventListener("jakie wydarzenie", "co ma sie stac");
+
+
+// btn.addEventListener('click', function() {
+//     console.log("klik!")
+//     btn.style.backgroundColor = "red";
+// })
+
+
+// btn.addEventListener('mouseover', function(){
+//     console.log("najechane")
+// })
+
+
+// btn.addEventListener('mousemove', function(){
+//     btn.style.backgroundColor = "red";
+//     console.log("move");
+// })
+
+
+// btn.addEventListener('mousemove', function(){
+//   alert("kliknaless") 
+// })
+
+
+let clickNumbers = 0;
+btn.addEventListener('click', function(){
+    clickNumbers += 1;
+    console.log("Click number: ", clickNumbers);
+})
+
+// window.addEventListener('scroll', function() {
+//     console.log("skrolujesz");
+// })
+
+// document.addEventListener('scroll', function(){
+//     console.log("skrolujesz o " + window.scrollY + "px");
+// })
+
+
+// function showScrollSize() {
+//     console.log("Skrolujesz o " + window.scrollY + "px");
+// }
+
+// window.addEventListener('scroll', showScrollSize);
+
+///////////////////////////////////////////////////////
+const spanResult = document.querySelector('span');
+
+function changeStateOfSpan() {
+    spanResult.textContent = window.scrollY;
+}
+
+window.addEventListener('scroll', changeStateOfSpan);
+
---
+++ 10d_js_intro/js/script.js
@@ -0,0 +1,58 @@
+// console.log("ok - script");
+
+// var a = 3;
+// console.log(typeof a);
+
+// var bo = true;
+// console.log(typeof bo);
+
+// var c = null;
+// console.log(typeof c);
+
+// var d;
+// console.log(typeof d);
+
+// var z = [];
+// var arr = [3, 1, 'test', z = [3,2][1]];
+// console.log(arr);
+// console.log(typeof arr);
+
+// var myFunction = function(){};
+
+// var myObj = {};
+
+
+// const result = document.querySelector(".description");
+// console.log(result);
+// console.log(result.id);
+// console.log(result.className);
+
+// let paragpraph = document.querySelectorAll('p');
+// console.log(paragpraph);
+
+// document.body;
+// const body1 = document.querySelector('body');
+// console.log(body1);
+
+// const body2 = document.body;
+// console.log(body2);
+
+// var text = document.querySelector('body > p.description.red');
+// console.log(text);
+
+
+// WYSZUKIWANIE PO ID
+// var item1 = document.getElementById("lorem1");
+// console.log(item1);
+console.log(window);
+console.log(document);
+console.log(document.body);
+// HTML
+console.log(document.documentElement);
+// IMG
+console.log(document.images);
+console.log(document.querySelectorAll('img'));
+
+document.querySelector('element');
+document.querySelectorAll('elementy');
+
