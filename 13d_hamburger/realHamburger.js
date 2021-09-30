f94acd163f2371158257ceb9f0b56d6f82b3eaaa
cymande0 <konradpietapl@gmail.com>
2021-09-30 22:54:50 +0200

    Loop JS

--- 13d_hamburger/realHamburger.js
+++ 13d_hamburger/realHamburger.js
@@ -1,10 +1,31 @@
+// JS
+// Easy sposob
+
+// const burger = document.querySelector(".burger");
+// const iconBurger = document.querySelector(".fa-bars");
+// const iconX = document.querySelector(".fa-times");
+// const column = document.querySelector("aside");
+// const wraper = document.querySelector(".wrapper");
+
+// burger.addEventListener("click", function() {
+//     iconBurger.classList.toggle("show");
+//     iconX.classList.toggle("show");
+//     column.classList.toggle("show");
+//     wraper.classList.toggle("show");
+// })
+
+// Trudniejszy sposób
 const burger = document.querySelector(".burger");
-const iconBurger = document.querySelector(".fa-bars");
-const iconX = document.querySelector(".fa-times");
-const column = document.querySelector("aside");
+const activeElement = document.querySelectorAll(".active");
 
 burger.addEventListener("click", function() {
-    iconBurger.classList.toggle("show");
-    iconX.classList.toggle("show");
-    column.classList.toggle("show");
+    for(let i = 0; i < activeElement.length; i++){
+        activeElement[i].classList.toggle("show");
+    }
 })
+
+
+// JQ
+// $(".burger").on("click", function(){
+//     $(".fas, aside, .wrapper").toggleClass("show");
+// })
