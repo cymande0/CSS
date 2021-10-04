3744424e9652f529ff1123a7c2dc2b8f3fe25f43
cymande0 <konradpietapl@gmail.com>
2021-10-04 23:05:18 +0200

    Przewijanie

---
+++ 14d_popupEfektyPrzyPrzewijaniu/js/przewijanieStronkiDoSekcji.js
@@ -0,0 +1,40 @@
+// Pierwszy sposób
+
+// $(".me").on("click", function(){
+//     $("body, html").animate({
+//         scrollTop: $(".s1").offset().top
+//     })
+// })
+
+// $(".portolio").on("click", function(){
+//     $("body, html").animate({
+//         scrollTop: $(".s2").offset().top
+//     })
+// })
+
+// $(".offer").on("click", function(){
+//     $("body, html").animate({
+//         scrollTop: $(".s3").offset().top
+//     })
+// })
+
+// $(".contact").on("click", function(){
+//     $("body, html").animate({
+//         scrollTop: $(".s4").offset().top
+//     })
+// })
+
+// Szybsza opcja
+// $("nav a").on("click", function() {
+//    const goToSection = "#" + $(this).attr("class");
+//    $("body, html").animate({
+//       scrollTop: $(goToSection).offset().top
+//    })
+// })
+
+$("nav a").on("click", function() {
+   const goToSection = "[data-section=" + $(this).attr("class") + "]";
+   $("body, html").animate({
+      scrollTop: $(goToSection).offset().top
+   })
+})
